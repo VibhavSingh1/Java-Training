30978540d9b2750470458c2fb79f56c05ff8f0a1
VibhavSingh1 <vibhav.singh0v@gmail.com>
2022-01-24 14:22:18 +0530

    First Commit

---
+++ training1/Print1.java
@@ -0,0 +1,11 @@
+package training1;
+
+public class Print1 {
+
+	public static void main(String[] args) {
+		int a = 10; int b = 5;
+		System.out.println("Sum " + (a + b));
+		
+	}
+
+}
---
+++ training1/display.java
@@ -0,0 +1,13 @@
+package training1;
+
+public class display
+{
+	public static void main(String[] ar)
+	{
+		String nm = "vanshika singh";
+		String ag = "13";
+		System.out.println("name " + nm);
+		System.out.println("age " + ag);
+	}
+}
+
